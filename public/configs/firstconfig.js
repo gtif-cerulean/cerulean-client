6f978b4bab6defed41aed48671e4eb2cd3db9c25
santilland <daniel.santillan@eox.at>
2025-01-16 10:45:04 +0100

    chore: updated eodash, adapted config

--- public/configs/firstconfig.js
+++ public/configs/firstconfig.js
@@ -68,32 +68,13 @@ export default {
                 id: "Information",
                 title: "Information",
                 layout: { x: 9, y: 0, w: 3, h: 4 },
-                type: "web-component",
+                type: "internal",
                 widget: {
-                  link: "https://cdn.skypack.dev/@eox/stacinfo",
+                  name: "EodashStacInfo",
                   properties: {
-                    for: store.states.currentUrl,
-                    allowHtml: "true",
-                    styleOverride:
-                      `.single-property {columns: 1!important;}
-                      h1 {margin:0px!important;font-size:16px!important;}
-                      header h1:after {
-                        content:' ';
-                        display:block;
-                        border:1px solid #d0d0d0;
-                      }
-                      h2 {font-size:15px}
-                      h3 {font-size:14px}
-                      summary {cursor: pointer;}
-                      #properties li > .value { font-weight: normal !important;}
-                      main {padding-bottom: 10px;}
-                      .footer-container {line-height:1;}
-                      .footer-container button {margin-top: -10px;}
-                      .footer-container small {font-size:10px;line-height:1;}`,
-                    header: '["title"]',
-                    body: "description",
+                    showIndicatorsBtn: false,
+                    showLayoutSwitcher: false,
                   },
-                  tagName: "eox-stacinfo",
                 },
               }
             : null;
@@ -124,19 +105,18 @@ export default {
         },
       },
       {
-        defineWidget: (indicator) => {
-          return indicator
+        defineWidget: (selectedSTAC) =>
+          selectedSTAC?.links.some((l) => l.rel === "service")
             ? {
-                id: "process",
-                layout: { x: 9, y: 4, w: 3, h: 8 },
-                title: "Process",
+                id: "Processes",
                 type: "internal",
+                title: "Processes",
+                layout: { x: 9, y: 4, w: 3, h: 8 },
                 widget: {
                   name: "EodashProcess",
                 },
               }
-            : null;
-        },
+            : null,
       },
       /*
       {
