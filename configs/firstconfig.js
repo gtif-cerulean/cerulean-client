d4d2b124ccee93da1e8d1c5bc371ee07fffeaf74
lubojr <lubojr@seznam.cz>
2025-12-16 10:00:27 +0000

    Deploying to gh-pages from @ gtif-cerulean/cerulean-client@c004f7a496222c4cbba987d686565380d3c4bfab 🚀

--- configs/firstconfig.js
+++ configs/firstconfig.js
@@ -46,7 +46,8 @@ export default {
             },
             btnsPosition: {
               x: "12/8/9",
-              y: "3/2/2",
+              y: 1,
+              gap: 32
             }
           },
         },
@@ -206,7 +207,8 @@ export default {
             },
             btnsPosition: {
               x: "12/9/10",
-              y: "3/2/2",
+              y: 1,
+              gap: 32
             }
           },
         },
